3b997400e817653eed87da56ac35298eb01b4ae2
hiroshi <hiroshi@looksrare.org>
2022-09-19 15:12:11 +0100

    fix: Filenames should be camelcase

--- src/utils/seaport/calculate-price-from-consideration.ts
+++
@@ -1,9 +0,0 @@
-import { SeaportConsideration } from "../../interfaces/seaport";
-import { BigNumber, constants } from "ethers";
-
-export default function calculatePriceFromConsideration(consideration: Array<SeaportConsideration>): BigNumber {
-  return consideration.reduce(
-    (sum: BigNumber, item: SeaportConsideration) => BigNumber.from(item.endAmount).add(sum),
-    constants.Zero
-  );
-}
