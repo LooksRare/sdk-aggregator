41eea3c19049260022cdf1ed0d481bfe5a87543c
hiroshi <hiroshi@looksrare.org>
2022-09-21 23:54:16 +0100

    test: Seaport order execution test

--- src/__tests__/aggregatorExecute.test.ts
+++ src/__tests__/aggregatorExecute.test.ts
@@ -8,6 +8,9 @@ import { addressesByNetwork, MakerOrder, generateMakerOrderTypedData } from "@lo
 import { constants } from "ethers";
 import { MakerOrderFromAPI } from "../interfaces/LooksRareV1";
 import calculateTxFee from "./helpers/calculateTxFee";
+import * as SeaportInterface from "../interfaces/Seaport";
+import { Seaport } from "@opensea/seaport-js";
+import { ItemType } from "@opensea/seaport-js/lib/constants";
 
 describe("LooksRareAggregator class", () => {
   let contracts: Mocks;
@@ -79,4 +82,65 @@ describe("LooksRareAggregator class", () => {
     const balanceAfterTx = await ethers.provider.getBalance(buyer.address);
     expect(balanceBeforeTx.sub(balanceAfterTx).sub(txFee)).to.equal(constants.WeiPerEther);
   });
+
+  it("can execute Seaport orders", async () => {
+    const chainId = SupportedChainId.MAINNET;
+    const signers = await getSigners();
+    const maker = signers.user1;
+    const buyer = signers.buyer;
+    const collection = contracts.collection1;
+    const addresses: Addresses = {
+      AGGREGATOR: contracts.looksRareAggregator.address,
+      LOOKSRARE_V1_PROXY: contracts.looksRareProxy.address,
+      SEAPORT_PROXY: contracts.seaportProxy.address,
+    };
+    const aggregator = new LooksRareAggregator(buyer, chainId, addresses);
+
+    const seaport = new Seaport(maker);
+
+    const blockNumber = await ethers.provider.getBlockNumber();
+    const block = await ethers.provider.getBlock(blockNumber);
+    const now = block.timestamp;
+
+    const { executeAllActions } = await seaport.createOrder(
+      {
+        startTime: now.toString(),
+        endTime: (now + 86400).toString(),
+        salt: "69420",
+        offer: [
+          {
+            itemType: ItemType.ERC721,
+            token: collection.address,
+            identifier: "1",
+          },
+        ],
+        consideration: [
+          {
+            amount: ethers.constants.WeiPerEther.toString(),
+            recipient: maker.address,
+          },
+        ],
+      },
+      maker.address
+    );
+    const order = (await executeAllActions()) as SeaportInterface.Order;
+    const tradeData = aggregator.transformSeaportListings([order]);
+
+    const balanceBeforeTx = ethers.utils.parseEther("2");
+
+    await ethers.provider.send("hardhat_setBalance", [
+      buyer.address,
+      balanceBeforeTx.toHexString().replace("0x0", "0x"),
+    ]);
+
+    const tx = await aggregator.execute([tradeData], buyer.address, true);
+
+    expect(await collection.ownerOf(1)).to.equal(buyer.address);
+    expect(await collection.balanceOf(buyer.address)).to.equal(1);
+
+    const txFee = await calculateTxFee(tx);
+    const balanceAfterTx = await ethers.provider.getBalance(buyer.address);
+    expect(balanceBeforeTx.sub(balanceAfterTx).sub(txFee)).to.equal(constants.WeiPerEther);
+  });
+
 });
