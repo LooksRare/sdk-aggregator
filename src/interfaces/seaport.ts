5b8d5d6520e267682f8f37a8203bb18526d4ca3c
hiroshi <hiroshi@looksrare.org>
2022-10-14 15:54:10 +0100

    docs: JSDoc

--- src/interfaces/seaport.ts
+++ src/interfaces/seaport.ts
@@ -1,22 +1,35 @@
 import { OrderType } from "@opensea/seaport-js/lib/constants";
 import { BigNumberish } from "ethers";
 
+/** The recipient of a sales' proceed */
 export interface Recipient {
+  /** Receive amount */
   amount: BigNumberish;
+  /** Recipient address */
   recipient: string;
 }
 
+/** Seaport order extra data object inside TradeData */
 export interface OrderExtraData {
+  /** Seaport trade numerator, it should be 1 for ERC721 and variable for ERC1155 */
   numerator: number;
+  /** Seaport trade denominator, it should be 1 for ERC721 and variable for ERC1155 */
   denominator: number;
+  /** Seaport order type */
   orderType: OrderType;
+  /** Seaport zone contract address */
   zone: string;
+  /** Seaport zone hash */
   zoneHash: string;
+  /** Seaport order salt */
   salt: string;
+  /** Seaport order conduit key */
   conduitKey: string;
+  /** Sale proceed recipients */
   recipients: Array<Recipient>;
 }
 
+/** Seaport order extra data schema */
 export const ORDER_EXTRA_DATA_SCHEMA = `
   tuple(
     uint120 numerator,
@@ -30,6 +43,7 @@ export const ORDER_EXTRA_DATA_SCHEMA = `
   ) orderExtraData
 `;
 
+/** Seaport extra data schema */
 export const EXTRA_DATA_SCHEMA = `
   tuple(
     tuple(uint256 orderIndex, uint256 itemIndex)[][] offerFulfillments,
