e3ff8a05a42683a8715b988f0c6f6c94052e5b01
hiroshi <hiroshi@looksrare.org>
2022-11-11 10:51:37 +0000

    test: Seaport ERC1155

--- src/__tests__/aggregatorExecute.test.ts
+++ src/__tests__/aggregatorExecute.test.ts
@@ -81,7 +81,7 @@ describe("LooksRareAggregator class", () => {
     return await aggregator.execute(tradeData, buyer.address, true);
   };
 
-  it("can execute LooksRare V1 orders (ERC-721)", async () => {
+  it("can execute LooksRare V1 orders (ERC721)", async () => {
     const collection = contracts.collection1;
     const signers = await getSigners();
     const buyer = signers.buyer;
@@ -103,7 +103,7 @@ describe("LooksRareAggregator class", () => {
     expect(balanceBeforeTx.sub(balanceAfterTx).sub(txFee)).to.equal(constants.WeiPerEther);
   });
 
-  it("can execute LooksRare V1 orders (ERC-1155)", async () => {
+  it("can execute LooksRare V1 orders (ERC1155)", async () => {
     const collection = contracts.collection3;
     const signers = await getSigners();
     const buyer = signers.buyer;
@@ -124,7 +124,7 @@ describe("LooksRareAggregator class", () => {
     expect(balanceBeforeTx.sub(balanceAfterTx).sub(txFee)).to.equal(constants.WeiPerEther);
   });
 
-  it("can execute Seaport orders", async () => {
+  it("can execute Seaport orders (ERC721)", async () => {
     const chainId = SupportedChainId.MAINNET;
     const signers = await getSigners();
     const maker = signers.user1;
@@ -179,6 +179,67 @@ describe("LooksRareAggregator class", () => {
 
     expect(await collection.ownerOf(1)).to.equal(buyer.address);
     expect(await collection.balanceOf(buyer.address)).to.equal(1);
+
+    const txFee = await calculateTxFee(tx);
+    const balanceAfterTx = await ethers.provider.getBalance(buyer.address);
+    expect(balanceBeforeTx.sub(balanceAfterTx).sub(txFee)).to.equal(constants.WeiPerEther);
+  });
+
+  it("can execute Seaport orders (ERC1155)", async () => {
+    const chainId = SupportedChainId.MAINNET;
+    const signers = await getSigners();
+    const maker = signers.user3;
+    const buyer = signers.buyer;
+    const collection = contracts.collection3;
+    const addresses: Addresses = {
+      AGGREGATOR: contracts.looksRareAggregator.address,
+      ERC20_ENABLED_AGGREGATOR: contracts.erc20EnabledLooksRareAggregator.address,
+      LOOKSRARE_V1_PROXY: contracts.looksRareProxy.address,
+      SEAPORT_PROXY: contracts.seaportProxy.address,
+    };
+    const aggregator = new LooksRareAggregator(buyer, chainId, addresses);
+
+    const seaport = new Seaport(maker);
+
+    const blockNumber = await ethers.provider.getBlockNumber();
+    const block = await ethers.provider.getBlock(blockNumber);
+    const now = block.timestamp;
+
+    const { executeAllActions } = await seaport.createOrder(
+      {
+        startTime: now.toString(),
+        endTime: (now + 86400).toString(),
+        salt: "69420",
+        offer: [
+          {
+            itemType: ItemType.ERC1155,
+            token: collection.address,
+            identifier: "1",
+            amount: "1",
+          },
+        ],
+        consideration: [
+          {
+            amount: ethers.constants.WeiPerEther.toString(),
+            recipient: maker.address,
+          },
+        ],
+      },
+      maker.address
+    );
+    const order = await executeAllActions();
+    const { tradeData } = await aggregator.transformListings({ seaport: [order], looksRareV1: [] });
+
+    const balanceBeforeTx = ethers.utils.parseEther("2");
+
+    await ethers.provider.send("hardhat_setBalance", [
+      buyer.address,
+      balanceBeforeTx.toHexString().replace("0x0", "0x"),
+    ]);
+
+    const tx = await aggregator.execute(tradeData, buyer.address, true);
+
+    expect(await collection.balanceOf(buyer.address, "1")).to.equal(1);
 
     const txFee = await calculateTxFee(tx);
     const balanceAfterTx = await ethers.provider.getBalance(buyer.address);
