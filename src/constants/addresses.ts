51db176376243815a75fa4946a6233ad2398e638
0xhiroshi <108636367+0xhiroshi@users.noreply.github.com>
2022-12-20 12:24:59 +0000

    chore: Update Goerli contract addresses (#24)

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -16,10 +16,10 @@ const mainnetAddresses: Addresses = {
 };
 
 const goerliAddresses: Addresses = {
-  AGGREGATOR: "",
-  ERC20_ENABLED_AGGREGATOR: "",
-  LOOKSRARE_V1_PROXY: "",
-  SEAPORT_PROXY: "",
+  AGGREGATOR: "0x8190FfdC71F7cBE4c09793138c2434D344f01B2c",
+  ERC20_ENABLED_AGGREGATOR: "0x733148c9b4a84C9ADff244D59b2A1dbdA5731472",
+  LOOKSRARE_V1_PROXY: "0xCf4a864398e75eb5540FF0B8817b7343BfFe0548",
+  SEAPORT_PROXY: "0x21b039a9E97c39DCB8cc01979eC3b91686f19e8f",
 };
 
 export const addressesByNetwork: { [chainId in SupportedChainId]: Addresses } = {
