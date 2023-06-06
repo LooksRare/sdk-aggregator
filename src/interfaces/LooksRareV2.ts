72ea998b3164ab7bdadf676dcde62c822e15ee23
0xBunta <bunta@looksrare.org>
2023-06-06 12:35:32 +0900

    chore: Referrer type add to MakerOrderFromAPI

--- src/interfaces/LooksRareV2.ts
+++ src/interfaces/LooksRareV2.ts
@@ -1,9 +1,15 @@
 import { MerkleTree, Maker } from "@looksrare/sdk-v2";
 import { BigNumberish, BytesLike } from "ethers";
 
+interface Referrer {
+  address: string;
+  rate: BigNumberish;
+}
+
 export interface MakerOrderFromAPI extends Maker {
   signature: string;
   merkleTree: MerkleTree;
+  referrer: Referrer;
 }
 
 /** LooksRare order extra data object inside TradeData */
